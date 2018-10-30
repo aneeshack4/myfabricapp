f8cfda6e32bd99ec332e88bf39d85ab6cb7dfe14
Aneesha Kommineni <aneeshack4@gmail.com>
2018-10-30 16:42:56 -0700

    simple fabric app; added a standard Fabric button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  DocumentCard,
+  DocumentCardPreview,
+  DocumentCardTitle,
+  DocumentCardActivity
+} from 'office-ui-fabric-react/lib/DocumentCard';
+import { DefaultButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
+import { Label } from 'office-ui-fabric-react/lib/Label';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <DocumentCard onClickHref='http://bing.com'>
+          <DocumentCardPreview
+            previewImages={ [
+              {
+                previewImageSrc: require('./documentpreview.png'),
+                iconSrc: require('./iconppt.png'),
+                width: 318,
+                height: 196,
+                accentColor: '#ce4b1f'
+              }
+            ] }
+          />
+          <DocumentCardTitle title='Revenue stream proposal fiscal year 2016 version02.pptx'/>
+          <DocumentCardActivity
+            activity='Created Feb 23, 2016'
+            people={
+              [
+                { name: 'Kat Larrson', profileImageSrc: require('./avatarkat.png') }
+              ]
+            }
+            />
+        </DocumentCard>
+        <Label>StandardButton</Label>
+        <DefaultButton
+          text="turn background red"
+          onClick={onClick}
+        />
+      </React.Fragment>
     );
+    function onClick() {
+      alert('Clicked');
+      document.body.style.backgroundColor = "red";
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Fabric><App /></Fabric>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
